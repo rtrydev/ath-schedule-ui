c5ed141c7e3c320c54b6e726c20cc91812a152a4
rtrydev <r.kulka44@gmail.com>
2023-03-23 18:23:41 +0100

    Added empty calendar, added footer

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./calendar.component.scss']
 })
 export class CalendarComponent {
+  hours: string[];
+
+  constructor() {
+    this.hours = this.generateHours();
+  }
+
+  private generateHours() {
+    const hours: string[] = [];
+
+    for (let i = 7; i < 22; i++) {
+      if (i > 9) {
+        hours.push(`${i}:00`);
+        continue;
+      }
+
+      hours.push(`0${i}:00`);
+    }
+
+    return hours;
+  }
 
 }
